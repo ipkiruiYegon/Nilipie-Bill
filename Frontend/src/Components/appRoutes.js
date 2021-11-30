010bb8b735473572bc1b8f09aa4ad11b929fe02e
Yegon Kipkirui Geoffrey <52694586+ipkiruiYegon@users.noreply.github.com>
2021-11-30 14:35:42 +0300

    update Frontend

--- Frontend/src/Components/appRoutes.js
+++ Frontend/src/Components/appRoutes.js
@@ -1,11 +1,16 @@
 import React, { useContext } from "react";
 import { useRoutes } from "react-router-dom";
+import { AuthContext } from "../App";
 
 import routes from "../routes";
 
 const AppRoutes = () => {
-  const isAuthenticated = false;
-  const token = "12345";
+  const { state } = useContext(AuthContext);
+  let token = "";
+  if (state.User && state.User.token) {
+    token = state.User.token;
+  }
+  const isAuthenticated = state.isAuthenticated ? state.isAuthenticated : false;
   const routing = useRoutes(routes(isAuthenticated, token));
   return <>{routing}</>;
 };
